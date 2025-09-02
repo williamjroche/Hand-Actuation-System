a220e6642f023a1edbbd0754340c1e8c172a3ddd
williamjroche <bjroche3@gmail.com>
2025-09-01 22:44:42 -0400

    optimized programs, same functionality, cleaner code

--- main.c
+++ main.c
@@ -1,106 +1,36 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
+//custom header files
 #include "servo.h"
-
-void servo1_position();
-void servo2_position();
-void servo3_position();
-void servo4_position();
-void servo5_position();
-void servo6_position();
+#include "servo_position.h"
 
 int main(void){
 	servo_setup();
 	
 	while(1){
-		//start servo1 at -90 deg, move to 90 deg
-		for(int pulse = 500; pulse <= 2500; pulse += 10){
-			servo1_position(pulse);
-			_delay_ms(10);
+		DDRB |= (1 << PORTB4) | (1 << PORTB5);
+		PORTC |= 0x01;
+		for(int counter = 0; counter <= 6400 ; counter++){
+			PORTC != PORTC;
+			_delay_ms(1);
 		}
-		//start servo2 at 90 deg, move to -90 deg
-		for(int pulse = 2500; pulse >= 500; pulse -= 10){
-			servo2_position(pulse);
-			_delay_ms(10);
-		}
-		//start servo3 at -90 deg, move to 90 deg
-		for(int pulse = 500; pulse <= 2500; pulse += 10){
-			servo3_position(pulse);
-			_delay_ms(9);
-		}
-		//start servo4 at 90 deg, move to -90 deg
-		for(int pulse = 2500; pulse >= 500; pulse -= 10){
-			servo4_position(pulse);
-			_delay_ms(9);
-		}
-		//start servo5 at -90 deg, move to 90 deg
-		for(int pulse = 500; pulse <= 2500; pulse += 10){
-			servo5_position(pulse);
-			_delay_ms(9);
-		}
-		//start servo6 at 90 deg, move to -90 deg
-		for(int pulse = 2500; pulse >= 500; pulse -= 10){
-			servo6_position(pulse);
-			_delay_ms(9);
-		}
-		//start servo1 at 90 deg, move to -90 deg
-		for(int pulse = 2500; pulse >= 500; pulse -= 10){
-			servo1_position(pulse);
-			_delay_ms(10);
-		}
-		//start servo2 at -90 deg, move to 90 deg
-		for(int pulse = 500; pulse <= 2500; pulse += 10){
-			servo2_position(pulse);
-			_delay_ms(10);
-		}
-		//start servo3 at 90 deg, move to -90 deg
-		for(int pulse = 2500; pulse >= 500; pulse -= 10){
-			servo3_position(pulse);
-			_delay_ms(9);
-		}
-		//start servo4 at -90 deg, move to 90 deg
-		for(int pulse = 500; pulse <= 2500; pulse += 10){
-			servo4_position(pulse);
-			_delay_ms(9);
-		}
-		//start servo5 at 90 deg, move to -90 deg
-		for(int pulse = 2500; pulse >= 500; pulse -= 10){
-			servo5_position(pulse);
-			_delay_ms(9);
-		}
-		//start servo6 at -90 deg, move to 90 deg
-		for(int pulse = 500; pulse <= 2500; pulse += 10){
-			servo6_position(pulse);
-			_delay_ms(9);
-		}
+		move_servo(1, 10, 500, 2500, true);//start servo1 at -90 deg, move to 90 deg
+		move_servo(2, 10, 2500, 500, false);//start servo2 at 90 deg, move to -90 deg
+		move_servo(3, 9, 500, 2500, true);//start servo3 at -90 deg, move to 90 deg
+		move_servo(4, 9, 2500, 500, false);//start servo4 at 90 deg, move to -90 deg
+		move_servo(5, 9, 500, 2500, true);//start servo5 at -90 deg, move to 90 deg
+		move_servo(6, 9, 2500, 500, false);//start servo6 at 90 deg, move to -90 deg
+		move_servo(1, 10, 2500, 500, false);//start servo1 at 90 deg, move to -90 deg
+		move_servo(2, 10, 500, 2500, true);//start servo2 at -90 deg, move to 90 deg
+		move_servo(3, 9, 2500, 500, false);//start servo3 at 90 deg, move to -90 deg
+		move_servo(4, 9, 500, 2500, true);//start servo4 at -90 deg, move to 90 deg
+		move_servo(5, 9, 2500, 500, false);//start servo5 at 90 deg, move to -90 deg
+		move_servo(6, 9, 500, 2500, true);//start servo6 at -90 deg, move to 90 deg
 	}
 	
 	return 0;
 }
 
-void servo1_position(int pulse_width){
-	//OCR1A because we need output control on PB1
-	OCR1A = pulse_width * 2; // pulse width is in micro seconds
-}
 
-void servo2_position(int pulse_width){
-	//OCR1B because we need output control on PB2
-	OCR1B = pulse_width * 2; // pulse width is in micro seconds
-}
-
-void servo3_position(int pulse_width){
-	OCR2A = pulse_width/16; // each tick is 16 microseconds
-}
-
-void servo4_position(int pulse_width){
-	OCR2B = pulse_width/16; // each tick is 16 microseconds
-}
-
-void servo5_position(int pulse_width){
-	OCR0A = pulse_width/16; // each tick is 16 microseconds
-}
-
-void servo6_position(int pulse_width){
-	OCR0B = pulse_width/16; // each tick is 16 microseconds
-}
---
+++ servo_position.h
@@ -0,0 +1,61 @@
+#ifndef servo_position_h
+#define servo_position_h
+
+void move_servo(int servo_number, int delay, int pulse, int pulse_end, bool to_pos){
+	if(to_pos == true){
+		for(pulse; pulse <= pulse_end; pulse+=10){
+			switch(servo_number){
+				case 1:
+					//OCR1A because we need output control on PB1
+					OCR1A = pulse * 2; // pulse width is in micro seconds
+					break;
+				case 2:
+					//OCR1B because we need output control on PB2
+					OCR1B = pulse * 2; // pulse width is in micro seconds
+					break;
+				case 3:
+					OCR2A = pulse / 16; // each tick is 16 microseconds
+					break;
+				case 4:
+					OCR2B = pulse/16; // each tick is 16 microseconds
+					break;
+				case 5:
+					OCR0A = pulse/16; // each tick is 16 microseconds
+					break;
+				case 6:
+					OCR0B = pulse/16; // each tick is 16 microseconds
+					break;
+			}
+			_delay_ms(delay);
+		}
+	}
+	else{
+		for(pulse; pulse >= pulse_end; pulse-=10){
+			switch(servo_number){
+				case 1:
+				//OCR1A because we need output control on PB1
+				OCR1A = pulse * 2; // pulse width is in micro seconds
+				break;
+				case 2:
+				//OCR1B because we need output control on PB2
+				OCR1B = pulse * 2; // pulse width is in micro seconds
+				break;
+				case 3:
+				OCR2A = pulse/16; // each tick is 16 microseconds
+				break;
+				case 4:
+				OCR2B = pulse/16; // each tick is 16 microseconds
+				break;
+				case 5:
+				OCR0A = pulse/16; // each tick is 16 microseconds
+				break;
+				case 6:
+				OCR0B = pulse/16; // each tick is 16 microseconds
+				break;
+			}
+			_delay_ms(delay);
+		}
+	}
+}
+
+#endif
